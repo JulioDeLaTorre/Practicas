1da622e6430ee8b914c7effa45bd403be804a29c
julioDeLaTorre <juliocesardelatorreavila8@gmail.com>
2025-09-11 20:09:27 -0600

    Practica3 se desarrollo una calculadora de isr y sueldo neto en base a las tablas del sat setptiembre2025

--- app/src/main/java/com/example/practicas/MainActivity.kt
+++ app/src/main/java/com/example/practicas/MainActivity.kt
@@ -1,144 +1,130 @@
 package com.example.practicas
 
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.OutlinedButton
-import androidx.compose.material3.OutlinedTextField
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.style.LineHeightStyle
-import com.example.practicas.ui.theme.PracticasTheme
-
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContent {
-            PracticasTheme {
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background){
-                    GreetingPractica()
+            MainScreen()
+        }
+    }
+}
+@Composable
+fun MainScreen() {
+    var salario by remember { mutableStateOf("") }
+    var isr by remember { mutableStateOf("") }
+    var neto by remember { mutableStateOf("") }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(20.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Top
+    ) {
+        Row {
+            Image(
+                painter = painterResource(id = R.drawable.satmalevolo),contentDescription = "satmalevoloxd",modifier = Modifier.size(200.dp)
+            )
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Row {
+            Texto(text = "Sueldo quincenal:",colorLetra = Color.White,fondo = Color.Blue)
+        }
+
+        Spacer(modifier = Modifier.height(8.dp))
+
+        Row {
+            TextField(
+                value = salario,
+                onValueChange = { it -> salario = it },
+                label = { Text("Ingresa sueldo") },
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+            )
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Row {
+            Button(onClick = {
+                val sueldo = salario.toDouble()
+                var A = 0.0
+                var B = 0.0
+
+                if (sueldo <= 368.10) {
+                    A = ((sueldo - 0.01) * 0.0192) + 0.00
+                } else if (sueldo <= 3124.35) {
+                    A = ((sueldo - 368.11) * 0.0640) + 7.05
+                } else if (sueldo <= 5490.75) {
+                    A = ((sueldo - 3124.36) * 0.1088) + 183.45
+                } else if (sueldo <= 6382.80) {
+                    A = ((sueldo - 5490.76) * 0.16) + 441.00
+                } else if (sueldo <= 7641.90) {
+                    A = ((sueldo - 6382.81) * 0.1792) + 583.65
+                } else if (sueldo <= 15412.80) {
+                    A = ((sueldo - 7641.91) * 0.2136) + 809.25
+                } else if (sueldo <= 24292.65) {
+                    A = ((sueldo - 15412.81) * 0.2352) + 2469.15
+                } else if (sueldo <= 46378.50) {
+                    A = ((sueldo - 24292.66) * 0.30) + 4557.75
+                } else if (sueldo <= 61838.10) {
+                    A = ((sueldo - 46378.51) * 0.32) + 11183.40
+                } else if (sueldo <= 185514.30) {
+                    A = ((sueldo - 61838.11) * 0.34) + 16130.55
+                } else if (sueldo >= 185514.31) {
+                    A = ((sueldo - 185514.31) * 0.35) + 58180.35
                 }
+                B = sueldo - A
+                isr = A.toString()
+                neto = B.toString()
+            }) {
+                Text("Calcular")
             }
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        Row {
+            Texto("ISR: ",Color.Black,Color.LightGray)
+            Texto(isr,Color.Black,Color.LightGray)
+        }
+
+        Spacer(modifier = Modifier.height(8.dp))
+        Row {
+            Texto("Neto: ",Color.Black,Color.Green)
+            Texto(neto,Color.Black,Color.Green)
         }
     }
 }
 
 @Composable
-fun GreetingPractica() {
-    //val context= LocalContext.current
-    //var texto by remember { mutableStateOf("") }
-    //var text by remember { mutableStateOf("") }
-    var ValorA by remember { mutableStateOf("") }
-    var ValorB by remember { mutableStateOf("") }
-    var Resultado by remember { mutableStateOf("") }
-
-    Column(
-        modifier = Modifier.fillMaxSize(),
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally
-    ){
-/*      Row(){
-            Text(text = "Escribe tu nombre:",
-                 color = Color.Green,
-                 fontFamily = FontFamily.SansSerif)
-
-        }
-
-        Row(){
-            OutlinedTextField(
-                value=texto,
-                label={Text("Nombre")},
-                onValueChange ={texto=it}
-            )
-        }
-
-        Row(){ //Codigo del profe para hacer una funcionalidad basica al boton
-            OutlinedButton(onClick = { /*TODO*/
-                Toast.makeText(
-                    context, //val context= LocalContext.current
-                    texto,
-
-                    Toast.LENGTH_LONG
-                ).show()
-            }) {
-                Text(text = "Enviar")
-            }
-        }
-
-        Row(Modifier.align(Alignment.CenterHorizontally )){
-            OutlinedTextField(
-                value = text,
-                label = {Text("ValorA")},
-                onValueChange = {text = it}
-            )
-        }
-
- */
-
-        Row(Modifier.align(Alignment.CenterHorizontally )){
-            OutlinedTextField(
-                value = ValorA,
-                label = {Text("ValorA")},
-                onValueChange = {ValorA = it}
-            )
-        }
-
-        Row(Modifier.align(Alignment.CenterHorizontally )){
-            OutlinedTextField(
-                value = ValorB,
-                label = {Text("ValorB")},
-                onValueChange = {ValorB = it}
-            )
-        }
-
-        Row(){
-            OutlinedButton(onClick = {
-                val A = ValorA.toInt()
-                val B = ValorB.toInt()
-                val C = A+B
-                Resultado = C.toString()
-            }) {
-                Text(text = "Sumar")
-            }
-
-            OutlinedButton(onClick = {
-                ValorA = ""
-                ValorB = ""
-                Resultado = ""
-            }) {
-                Text(text = "Borrar")
-            }
-        }
-
-        Row(Modifier.align(Alignment.CenterHorizontally )){
-            OutlinedTextField(
-                value = Resultado,
-                label = {Text("Resultado")},
-                onValueChange = {Resultado = it}
-            )
-        }
-
-
-    }
+fun Texto(text: String, colorLetra: Color, fondo: Color) {
+    Text(
+        text = text,
+        color = colorLetra,
+        modifier = Modifier
+            .background(fondo)
+            .padding(4.dp)
+    )
 }
+
+
+
+
