1a3040097e98cc1318ae848cc5383f664e673079
julioDeLaTorre <juliocesardelatorreavila8@gmail.com>
2025-10-02 16:57:33 -0600

    Practica2 1.0

--- app/src/main/java/com/example/practicas/MainActivity.kt
+++ app/src/main/java/com/example/practicas/MainActivity.kt
@@ -4,16 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
@@ -25,6 +27,8 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.unit.dp
 import com.example.practicas.ui.theme.PracticasTheme
 import java.util.Stack
@@ -50,89 +54,80 @@ fun MainScreen(){
     var display by remember { mutableStateOf(("")) }
 
     Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .padding(16.dp,16.dp,16.dp,40.dp),
+        modifier = Modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Bottom
+        verticalArrangement = Arrangement.SpaceEvenly
     ) {
-        // Pantalla
+        Spacer(modifier = Modifier.height(100.dp))
         Row(
             modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.Center
+            horizontalArrangement = Arrangement.End,
+            verticalAlignment = Alignment.Bottom
         ) {
             TextField(
                 value = display,
-                onValueChange = { it ->  display = it },
+                onValueChange = { it -> display = it },
                 modifier = Modifier.fillMaxWidth(),
-                readOnly = true
+                readOnly = true,
 
             )
         }
 
         Spacer(modifier = Modifier.height(16.dp))
-
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1f),
+            verticalArrangement = Arrangement.Bottom,
+        ) {
         Row(
             modifier = Modifier.fillMaxWidth(),
             horizontalArrangement = Arrangement.End
         ) {
-            Button(onClick = { display =  "" }) {
-                Text("AC")
-            }
-
-            Spacer(modifier = Modifier.width(32.dp))
-
-
+            Spacer(modifier = Modifier.weight(3f))
+            BotonCalculadora("AC", onClick = { display =  "" })
         }
-
-        Spacer(modifier = Modifier.height(16.dp))
 
         Row(
             modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.SpaceEvenly
+            horizontalArrangement = Arrangement.spacedBy(1.dp)
         ) {
-            Button(onClick = { display += "7" }) { Text("7") }
-            Button(onClick = { display += "8" }) { Text("8") }
-            Button(onClick = { display += "9" }) { Text("9") }
-            Button(onClick = { display += "*" }) { Text("*") }
+            BotonCalculadora("7",onClick = { display += "7" })
+            BotonCalculadora("8",onClick = { display += "8" })
+            BotonCalculadora("9",onClick = { display += "9" })
+            BotonCalculadora("*",onClick = { display += "*" })
         }
-
-        Spacer(modifier = Modifier.height(16.dp))
 
         Row(
             modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.SpaceEvenly
+            horizontalArrangement = Arrangement.spacedBy(1.dp)
         ) {
-            Button(onClick = { display += "4" }) { Text("4") }
-            Button(onClick = { display += "5" }) { Text("5") }
-            Button(onClick = { display += "6" }) { Text("6") }
-            Button(onClick = { display += "-" }) { Text("-") }
+            BotonCalculadora("4",onClick = { display += "4" })
+            BotonCalculadora("5",onClick = { display += "5" })
+            BotonCalculadora("6",onClick = { display += "6" })
+            BotonCalculadora("-",onClick = { display += "-" })
         }
-
-        Spacer(modifier = Modifier.height(16.dp))
 
         Row(
             modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.SpaceEvenly
+            horizontalArrangement = Arrangement.spacedBy(1.dp)
         ) {
-            Button(onClick = { display += "1"}) { Text("1") }
-            Button(onClick = { display += "2"}) { Text("2") }
-            Button(onClick = { display += "3"}) { Text("3") }
-            Button(onClick = { display += "+"}) { Text("+") }
+            BotonCalculadora("1",onClick = { display += "1" })
+            BotonCalculadora("2",onClick = { display += "2" })
+            BotonCalculadora("3",onClick = { display += "3" })
+            BotonCalculadora("+",onClick = { display += "+" })
         }
-
-        Spacer(modifier = Modifier.height(16.dp))
 
         Row(
             modifier = Modifier.fillMaxWidth(),
-            horizontalArrangement = Arrangement.SpaceEvenly
+            horizontalArrangement = Arrangement.spacedBy(1.dp)
         ) {
-            Button(onClick = { display += "0" }) { Text("0") }
-            Button(onClick = { display += "0" }) { Text(".") }
-            Button(onClick = { display = evaluarExpresion(display) }) { Text("=")}
-            Button(onClick = { display += "/" }) { Text("/") }
+            BotonCalculadora("0",onClick = { display += "0" })
+            BotonCalculadora(".",onClick = { display += "." })
+            BotonCalculadora("=", onClick = { display = evaluarExpresion(display) })
+            BotonCalculadora("/",onClick = { display += "/" })
         }
-    }
+    }}
 }
 
 fun convertirAPostfija(expresion: String): List<String> {
@@ -195,5 +190,25 @@ fun evaluarExpresion(expresion: String): String {
         resultado.toString()
     } catch (e: Exception) {
         "Error"
+    }
+}
+@Composable
+fun BotonCalculadora(
+    symbol: String,
+    onClick: () -> Unit,
+) {
+    Button(
+        onClick = onClick,
+        modifier = Modifier
+            .width(89.3.dp) // Ancho fijo para cada botón
+            .aspectRatio(1f),
+        colors = ButtonDefaults.buttonColors(
+            containerColor = Color.Transparent, // Fondo transparente (sin relleno)
+            contentColor = Color.Black // Color del texto en negro
+        ),
+        border = BorderStroke(1.dp, Color.Black), // Borde negro de 1dp
+        shape = RectangleShape // Esto asegura una forma cuadrada o ligeramente redondeada, no circular
+    ) {
+        Text(symbol)
     }
 }
