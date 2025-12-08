86d4399444da88a743368b1c9aec78468120b1c9
julioDeLaTorre <juliocesardelatorreavila8@gmail.com>
2025-12-07 22:18:10 -0600

    Practica FirebaseNotes 1.0

--- app/src/main/java/com/example/practicas/secrets.kt
+++ app/src/main/java/com/example/practicas/secrets.kt
@@ -4,4 +4,4 @@ object Secrets {
     // Pon aquí tus claves reales
     const val TWILIO_SID = "AC698e69b2cdc74f74a51"+"ee18a90b82788"
     const val TWILIO_TOKEN = "6dbbda7284d94ad14432"+"978904c6ac35"
-}
+}//
